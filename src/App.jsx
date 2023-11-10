25edac8b59ba118f174097c0b02f0d2c23910985
BridgetAamana <amanabridget03@gmail.com>
2023-11-10 12:51:26 +0100

    Updated Layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Mobiledivider from "./assets/pattern-divider-mobile.svg";
 
 function App() {
   const [advice, setAdvice] = useState("");
+  const [adviceNumber, setAdviceNumber] = useState(117);
 
   const fetchAdvice = () => {
     fetch("https://api.adviceslip.com/advice")
@@ -11,6 +12,7 @@ function App() {
       .then((data) => {
         const adviceText = data.slip.advice;
         setAdvice(adviceText);
+        setAdviceNumber(adviceNumber + 1);
       })
       .catch((error) => {
         console.error("Error fetching advice:", error);
@@ -28,17 +30,21 @@ function App() {
   return (
     <div className="bg-slate-900 w-screen h-screen flex flex-col justify-center p-4 ">
       <main className="bg-gray-700 px-8 pt-8 pb-12 max-w-sm rounded-xl mx-auto flex flex-col space-y-6 relative">
-        <p className="text-green-400 text-sm text-center">ADVICE #117</p>
-        <p className="text-slate-100 text-2xl text-center">
+        <p className="text-green-400 text-sm text-center">ADVICE #{adviceNumber}</p>
+        <p className="text-slate-100 text-2xl text-center flex flex-wrap">
           {advice}
         </p>
-        <div>
+        <div className="flex justify-center">
           <img src={Mobiledivider} alt="Mobiledivider" />
         </div>
-        <div
-          className="bg-green-300 rounded-full flex justify-center w-14 absolute -bottom-8 right-36 hover:bg-green-400 hover:shadow-2xl hover:shadow-green-500 cursor-pointer md:right-44"
-        >
-          <img src={Dice} alt="Dice" className="p-4" onClick={handleDiceClick} />
+        <div className="relative">
+          <div className="bg-gray relative">
+            <div className="absolute inset-x-0  -bottom-20 flex justify-center ">
+              <div className="bg-green-400 rounded-full w-14 cursor-pointer hover:bg-green-400 hover:shadow-2xl hover:shadow-green-500 ">
+                <img src={Dice} alt="Dice" className="p-4 " onClick={handleDiceClick} />
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
